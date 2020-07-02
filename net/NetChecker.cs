5f040a7e8e33438add9497078f6da21ec941934a
jupark33 <jupark33@gmail.com>
2020-07-02 14:58:24 +0900

    Update NetChecker.cs

--- net/NetChecker.cs
+++ net/NetChecker.cs
@@ -45,5 +45,57 @@ namespace NetCheck.net
       
       return result;
     }
+    
+    public bool getCheckSocketResult(NetCheckModel model) 
+    {
+      bool result = false;
+      
+      var exceptions = new ConcurrentQueue<Exceptions>();
+      try
+      {
+        for (int i = 0; i < model.retryCnt; i++)
+        {
+            Socket sock = null;
+            try
+            {
+              logger.Debug("NetChecker.getCheckSocketResult, TRY COUNT (" + model.port + ") : " + (i + 1));
+              sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+              var ep = new IPEndPoint(IPAddress.Parse(model.ip), model.port);
+              sock.Connect(ep);
+              
+              result = sock.Connected;
+              logger.Debug("NetChecker.getCheckSocketResult, getCheckSocket : " + result);
+              if (result) break;
+              
+              sock.Shutdown(SocketShutdown.Both);
+            }
+            catch (Exception e) 
+            {
+              exceptions.Enqueue(e);
+            }
+            finally
+            {
+              if (sock != null)
+              {
+                sock.Close();
+                logger.Debug("NetChecker.getCheckSocketResult, sock.Close() called ");                
+              }
+            }
+        }
+        throw new AggregateException(exceptions);
+      }
+      catch (AggregateException e)
+      {
+        bool existException = false;
+        foreach (var item in e.InnerExceptions)
+        {
+            existException = true;
+            logger.Debug("NetChecker.getCheckSocketResult, AggregateException : " + item.Message);
+        }
+        return = !existException;
+      }
+      
+      return result;
+    }
   }
 }
